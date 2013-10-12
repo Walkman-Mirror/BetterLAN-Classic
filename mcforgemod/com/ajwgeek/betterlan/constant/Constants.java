92a950ee728209182e51905811f8fec36f885443
Austin Walhof <ajwgeek@gmail.com>
2013-10-12 14:45:40 -0500

    Server download update
    Fixes #4

--- mcforgemod/com/ajwgeek/betterlan/constant/Constants.java
+++ mcforgemod/com/ajwgeek/betterlan/constant/Constants.java
@@ -8,14 +8,14 @@ public class Constants
 	public static final String modVersion           = "v1.0.6"; //Major Minor Revision
 	public static final String modID                = "BetterLAN";
 	public static final String modName              = "BetterLAN";
-	public static final String serverDownloadURL    = "https://dl.dropbox.com/s/xjn4cxgh1ciay4u/1.6.4.jar";
-	public static final String configDownloadURL    = "https://dl.dropbox.com/s/ooly79c3pexr4ii/configurations.zip";
-	public static final String pluginURL            = "https://dl.dropbox.com/s/x53p6o8zm80nxqe/plugin.jar";
-	public static final String jglicense            = "https://dl.dropbox.com/s/epwm6dea2y460nm/JG-LICENSE.txt";
-	public static final String cblicense            = "https://dl.dropbox.com/s/ukgfonkf43aujw6/CB-LICENSE.txt";
-	public static final String serverJarName        = "106.jar";
-	public static final String pName                = "betterlan-1.0.6.jar";
-	public static final String settingsFile         = "betterlan_settings_1_0_6.cfg";
+	public static final String serverDownloadURL    = "https://dl.dropbox.com/s/wv4ek18fzbsvb1b/CraftBukkit-1.6.4.jar";
+	public static final String configDownloadURL    = "https://dl.dropbox.com/s/9lb7marvt5v6tv5/CraftBukkit-1.6.4-Configurations.zip";
+	public static final String pluginURL            = "https://dl.dropbox.com/s/1adglxcl67ajxdd/BetterLAN-v1.0.6-Plugin.jar";
+	public static final String jglicense            = "https://dl.dropbox.com/s/cq3kdhe6oe99tj7/JG-LICENSE.txt";
+	public static final String cblicense            = "https://dl.dropbox.com/s/8qprfe7r2valj19/CB-LICENSE.txt";
+	public static final String serverJarName        = "Bukkit-164-106.jar";
+	public static final String pName                = "BetterLAN-v1.0.6-Plugin.jar";
+	public static final String settingsFile         = "betterlan_settings.cfg";
 	public static final String jgLicenseFile        = "JGroups_License.txt";
 	public static final String cbLicenseFile        = "CraftBukkit_License.txt";
 	//End Updated Every Release
